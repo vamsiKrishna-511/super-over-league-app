700e8af01e6bb6875c4ae43f9959216b2346232a
vamsiKrishna-511 <devulapallivamsi197@gmail.com>
2022-12-28 09:43:01 +0000

    super over league

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="super-over-league-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image1"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image1"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
